d4d1f910d8615e7bdb03b2e234a9391c60045294
IanKamau <theurikamau7@gmail.com>
2023-10-08 02:11:13 +0300

    completed sequences assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0,1
+    numbers = []
+    for _ in range (length):
+        
+        numbers.append(a)
+        a,b = b, a+b
+    print(numbers,end ='\n')
+        
+print_fibonacci(2)
